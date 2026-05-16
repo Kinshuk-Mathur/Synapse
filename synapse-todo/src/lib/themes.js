0315b2bf38d0d49aed859d3a7d6fa6260be79588
KINSHUK <kinshukmathur46@gmai.com>
2026-05-16 11:29:03 +0530

    Remove synapse todo folder

--- synapse-todo/src/lib/themes.js
+++
@@ -1,6 +0,0 @@
-export const synapseThemes = [
-  { id: "obsidian", name: "Obsidian Neon", tone: "Default OS" },
-  { id: "midnight", name: "Midnight Tech", tone: "Deep Focus" },
-  { id: "inferno", name: "Inferno Focus", tone: "Exam Sprint" },
-  { id: "pink", name: "Pink Aura", tone: "Dark Bloom" }
-];
