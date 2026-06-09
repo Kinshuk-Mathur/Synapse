345b3d1775e9f9f959b8957d0af9c3d4fa3d2743
KINSHUK <kinshukmathur46@gmai.com>
2026-06-09 17:03:59 +0530

    remove the custom cursor

--- synapse-dashborad/src/app/providers.jsx
+++ synapse-dashborad/src/app/providers.jsx
@@ -2,15 +2,11 @@
 
 import { AuthProvider } from "../context/AuthContext";
 import { NotificationsProvider } from "../context/NotificationsContext";
-import CustomCursor from "../components/CustomCursor";
 
 export default function Providers({ children }) {
   return (
-    <>
-      <CustomCursor />
-      <AuthProvider>
-        <NotificationsProvider>{children}</NotificationsProvider>
-      </AuthProvider>
-    </>
+    <AuthProvider>
+      <NotificationsProvider>{children}</NotificationsProvider>
+    </AuthProvider>
   );
 }
